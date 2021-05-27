10deb3a1807173f85f2834a06ca1153c37fc3f42
Gabriel Ribeiro <gabrielkaina99@gmail.com>
2021-05-27 13:46:48 -0300

    grid two columns component

---
+++ src/components/GridTwoColumn/mock.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'Grid Two Column',
+  text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque, dolorem quibusdam, illo vel iste accusamus sequi placeat, voluptatem ea animi error sit dicta? Quia placeat aut optio sint sed. Suscipit.',
+  srcImg: 'assets/images/javascript.svg',
+};
